033342347570f2e816f59e2b6c1ca54c1143ad61
Thisalzzz <wasanthaakurgoda@gmail.com>
2025-04-15 13:57:03 +0530

    Initial React setup with Vite and MUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Furniture Design App
+      </Typography>
+      <Typography>Designer login placeholder</Typography>
+    </Container>
+  );
+}
+export default App;
